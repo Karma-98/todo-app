92073e32a3b71399690bcc7278754c15fd6f54bd
Karma-98 <limyk2014@gmail.com>
2020-03-18 15:58:36 +0800

    Refactor home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -18,14 +20,29 @@ class NewVisitorTest(unittest.TestCase):
         # He notices the the title named To-Do to confirm
         # he is at the right url
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
 
         # He is prompted to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types "Complete CS50" into a text box
+        inputbox.send_keys('Complete CS50')
 
         # When he hits enter, the page updates and now the page lists:
-        # 1. Complete CS50 in his to-do list
+        # 1: Complete CS50 in his to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            # List comprehension to return true or false, then any return it
+            # as boolean
+            any(rows.text == '1: Complete CS50' for row in rows)
+        )
 
         # There is another text box prompting him to enter another
         # to-do item
@@ -36,6 +53,7 @@ class NewVisitorTest(unittest.TestCase):
         # He visited the site again and saw his list is still there
 
         # He then turn off his laptop, satisfied
+        self.fail('Finish the test!')
 
 
 if __name__ == "__main__":
